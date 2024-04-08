0d20f7d6e4ff2ddbe7724ff0b7dc1bb044f04693
hyobeom.seo <hyobeom.seo@samsung.com>
2024-04-08 22:28:40 +0900

    All kill~~~

--- 2captcha/sitc.js
+++ 2captcha/sitc.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer-extra');
 const stealthPlugin = require('puppeteer-extra-plugin-stealth');
+const Captcha = require("2captcha-ts");
+const solver = new Captcha.Solver("bd3d6a33966ff9f616fad8a5a302cfbc");
 
 async function runTest(){
     puppeteer.use(stealthPlugin());
     let lanchParam = {
         headless: false,
-        ignoreDefaultArgs: ['--enable-automation'],
-        args: ['--disable-blink-features=AutomationControlled'],
+       // ignoreDefaultArgs: ['--enable-automation'],
+       // args: ['--disable-blink-features=AutomationControlled'],
        // executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
     };
 
@@ -15,8 +17,8 @@ async function runTest(){
     let page = (await browser.pages())[0];
     //const page = await browser.newPage();
 
-    await page.setViewport({ width: 800, height: 600 });
-    await page.setDefaultNavigationTimeout(1 * 60 * 1000);
+    await page.setViewport({ width: 1024, height: 768 });
+    //await page.setDefaultNavigationTimeout(1 * 60 * 1000);
 
 
     await page.goto("https://api.sitcline.com/sitcline/query/cargoTrack");
@@ -35,8 +37,9 @@ async function runTest(){
     console.log(rect);
 
     
-    await page.screenshot({
-        path: 'captcha.png',
+    const base64Image = await page.screenshot({
+        //path: 'captcha.png',
+        encoding: 'base64',
         clip: {
             x: rect.left,
             y: rect.top,
@@ -45,7 +48,54 @@ async function runTest(){
         }
     });
     
+    //console.log(base64Image);
 
+    
+    const solveRes = await solver.imageCaptcha({
+        body: base64Image,
+        numeric: 3,
+        min_len: 4,
+        max_len: 4
+    });
+
+    console.log(solveRes);
+
+
+    const captchaSolve = solveRes.data;
+
+    // #app > div > div:nth-child(3) > div > div > div.el-dialog__body > form > div.el-form-item.el-form-item--feedback.is-error.is-required.el-form-item--medium > div > div.el-input.el-input--medium.el-input--prefix.el-input--suffix > input
+    // //*[@id="app"]/div/div[3]/div/div/div[2]/form/div[1]/div/div[1]/input
+
+    // ID
+    //const eInputId = await page.waitForXPath('//*[@id="app"]/div/div[3]/div/div/div[2]/form/div[1]/div/div[1]/input');
+    //await eInputId.type('thinktip');    
+    const eInputId = await page.waitForSelector('::-p-xpath(//*[@id="app"]/div/div[3]/div/div/div[2]/form/div[1]/div/div[1]/input)');
+    await eInputId.type('thinktip');
+
+
+    // PW
+    // #app > div > div:nth-child(3) > div > div > div.el-dialog__body > form > div:nth-child(2) > div > div.el-input.el-input--medium.el-input--prefix.el-input--suffix > input
+    // //*[@id="app"]/div/div[3]/div/div/div[2]/form/div[2]/div/div[1]/input
+    const eInputPw = await page.waitForSelector('::-p-xpath(//*[@id="app"]/div/div[3]/div/div/div[2]/form/div[2]/div/div[1]/input)');
+    await eInputPw.type('thinktip1234');
+    
+    // Captcha
+    // #app > div > div:nth-child(3) > div > div > div.el-dialog__body > form > div:nth-child(3) > div > div > input
+    // //*[@id="app"]/div/div[3]/div/div/div[2]/form/div[3]/div/div/input
+    const eInputCaptcha = await page.waitForSelector('::-p-xpath(//*[@id="app"]/div/div[3]/div/div/div[2]/form/div[3]/div/div/input)');
+    await eInputCaptcha.type(captchaSolve);
+
+    // Button
+    // #app > div > div:nth-child(3) > div > div > div.el-dialog__body > form > div:nth-child(5) > div > button
+    // //*[@id="app"]/div/div[3]/div/div/div[2]/form/div[5]/div/button
+    const eButtonLogin = await page.waitForSelector('::-p-xpath(//*[@id="app"]/div/div[3]/div/div/div[2]/form/div[5]/div/button)');
+    await eButtonLogin.click();
+
+
+    
+    const eInputBl = await page.waitForSelector('::-p-xpath(//*[@id="app"]/div/div[2]/div/div[1]/form/div/div[2]/div/div/div[1]/input)');
+    await eInputBl.type('BL');
+    
 }
 
 runTest();
